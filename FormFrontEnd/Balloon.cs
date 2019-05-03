688f2346016c978b1e66f1bdcacb85922f3c7b9f
BenJokela <benjokela@gmail.com>
2019-05-03 08:24:48 -0300

    initial code

---
+++ FormFrontEnd/Balloon.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormFrontEnd
+{
+    public class Balloon
+    {
+        private double height;
+        private double diameter;
+        private string colour;
+
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                height = value;
+            }
+        }
+        public double Diameter
+        {
+            get
+            {
+                return diameter;
+            }
+            set
+            {
+                diameter = value;
+            }
+        }
+        public string Colour
+        {
+            get
+            {
+                return colour;
+            }
+            set
+            {
+                if(value.ToUpper() == "RED"|| value.ToUpper() == "BLUE")
+                {
+                    colour = value;
+
+                }
+                else
+                {
+                    throw new DataException("Colour must be blue or red");
+                }
+            }
+        }
+
+
+    }
+}
--- FormFrontEnd/Form1.cs
+++ FormFrontEnd/Form1.cs
@@ -11,7 +11,8 @@ using System.Windows.Forms;
 namespace FormFrontEnd
 {
     public partial class Form1 : Form
-    {     
+    {
+        Balloon myBalloon;
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +20,33 @@ namespace FormFrontEnd
 
         private void btnSet_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                myBalloon = new Balloon()
+                {
+                    Colour = txtColor.Text,
+                    Diameter = Convert.ToDouble(txtDiameter.Text),
+                    Height = Convert.ToDouble(txtHeight.Text)
+
+                };
+                //myBalloon.Height = Convert.ToDouble(txtHeight.Text);
+                //myBalloon.Diameter = Convert.ToDouble(txtDiameter.Text);
+                //myBalloon.Colour = txtColor.Text;
+            }
+            catch (DataException da)
+            {
+                MessageBox.Show(da.Message, da.GetType().ToString(),MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
         }
 
         private void btnGet_Click(object sender, EventArgs e)
         {
-            
+            lblOutput.Text = $"Colour: { myBalloon.Colour} \nHeight: {myBalloon.Height}\nDiameter: {myBalloon.Diameter}";
         }
     }
 }
